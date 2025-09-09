2da65d6464e46945244ca886e3dba064ae2aad89
NikolausWeiss <35804118+NikolausWeiss@users.noreply.github.com>
2025-09-09 17:30:10 +0200

    fixes to file paths

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Register the service worker for offline capabilities
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
         .then(reg => console.log('Service Worker registered', reg))
         .catch(err => console.error('Service Worker registration failed', err));
 }
